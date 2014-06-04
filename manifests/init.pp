bdd3ea24ccd6fe15218dce7e0d59c52453536220
Chris Cowley <chris@chriscowley.me.uk>
2014-06-04 17:09:10 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
 class yumrepos {
   if $osfamily == 'RedHat' {
     include yumrepos::epel
-    include yumrepos::chriscowley
     include yumrepos::puppetlabs
   } else {
     notice ("Your operating system ${::operatingsystem} is not supported for EPEL repositories")
