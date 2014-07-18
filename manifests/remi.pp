f1f92245ed2362e8e37378c0c743fb9435123bfc
¨Chris <ccowley@b-com.com>
2014-07-18 16:03:47 +0200

    Added Remi Collet repo

---
+++ manifests/remi.pp
@@ -0,0 +1,24 @@
+class yumrepos::chriscowley {
+  yumrepo { "remi":
+    mirrorlist => "http://rpms.famillecollet.com/enterprise/${::os_maj_version}/remi/mirror",
+    descr    => "Les RPM de remi pour Enterprise Linux 6 - ${::architecture}",
+    enabled  => 1,
+    gpgcheck => 1,
+    gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-ChrisCowley",
+    require  => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-ChrisCowley"],
+  }
+
+  file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-remi":
+    ensure => present,
+    owner  => "root",
+    group  => "root",
+    mode   => "0644",
+    source => "puppet:///modules/yumrepos/RPM-GPG-KEY-remi",
+  }
+
+  yumrepos::rpm_gpg_key {"RPM-GPG-KEY-remi":
+    path     => "/etc/pki/rpm-gpg/RPM-GPG-KEY-remi",
+    require  => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-remi"]
+  }
+}
+
