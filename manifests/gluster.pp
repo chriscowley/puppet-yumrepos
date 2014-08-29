ad240e2709ea138200daa2be3bb045d55632b146
Chris Cowley <chris@chriscowley.me.uk>
2014-08-29 21:20:16 +0200

    Updated Gluster-epel repo to latest

--- manifests/gluster.pp
+++ manifests/gluster.pp
@@ -1,18 +1,18 @@
 class yumrepos::gluster {
   yumrepo { "glusterfs-epel":
-    baseurl  => "http://download.gluster.org/pub/gluster/glusterfs/3.3/3.3.1/EPEL.repo/epel-${::os_maj_version}/${::architecture}",
+    baseurl  => "http://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/epel-${::os_maj_version}/${::architecture}",
     descr    => "name=GlusterFS is a clustered file-system capable of scaling to several petabytes.",
     enabled  => 1,
     gpgcheck => 0,
   }
   yumrepo { "glusterfs-swift-epel":
-    baseurl  => "http://download.gluster.org/pub/gluster/glusterfs/3.3/3.3.1/EPEL.repo/epel-${::os_maj_version}/${::architecture}",
+    baseurl  => "http://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/epel-${::os_maj_version}/${::architecture}",
     descr    => "name=GlusterFS is a clustered file-system capable of scaling to several petabytes.",
     enabled  => 1,
     gpgcheck => 0,
   }
   yumrepo { "glusterfs-source-epel":
-    baseurl  => "http://download.gluster.org/pub/gluster/glusterfs/3.3/3.3.1/EPEL.repo/epel-${::os_maj_version}/${::architecture}",
+    baseurl  => "http://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/epel-${::os_maj_version}/${::architecture}",
     descr    => "name=GlusterFS is a clustered file-system capable of scaling to several petabytes.",
     enabled  => 1,
     gpgcheck => 0,
