a357c81f495305d73f879191014f1771fa24872d
Chris Cowley <chris@chriscowley.me.uk>
2014-10-09 21:47:54 +0200

    Corrected Remi repo config

--- manifests/remi.pp
+++ manifests/remi.pp
@@ -4,8 +4,8 @@ class yumrepos::remi {
     descr    => "Les RPM de remi pour Enterprise Linux 6 - ${::architecture}",
     enabled  => 1,
     gpgcheck => 1,
-    gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-ChrisCowley",
-    require  => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-ChrisCowley"],
+    gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi",
+    require  => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-remi"],
   }
 
   file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-remi":
